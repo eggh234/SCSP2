3d13a731cbf02698ec4058a3888a8d09f8736147
eggh234 <61628697+eggh234@users.noreply.github.com>
2024-02-08 16:18:11 -0500

    Updated option 3

--- 2FA.py
+++ 2FA.py
@@ -17,47 +17,76 @@ def main():
         create_user.check_root_privileges()
 
         # Request input: username
-        create_user.uname = create_user.request_input(
-            "Enter Username you want to add", "username"
-        )
+        uname = create_user.request_input("Enter Username you want to add", "username")
 
         # Request input: password
-        create_user.password = create_user.request_input(
-            "Enter Password for the user", "password"
-        )
-        create_user.re_password = create_user.request_input(
+        password = create_user.request_input("Enter Password for the user", "password")
+        re_password = create_user.request_input(
             "Re-enter Password for the user", "password"
         )
 
         # Verify that the passwords match
-        if create_user.password != create_user.re_password:
+        if password != re_password:
             print("Passwords do not match")
             sys.exit()
 
         # Request input: salt
-        create_user.salt = create_user.request_valid_salt()
+        salt = create_user.request_valid_salt()
 
         # Create new user with the provided input
-        create_user.user = create_user.User(
-            create_user.uname, create_user.password, create_user.salt
-        )
+        user = create_user.User(uname, password, salt)
 
         # Print all the user info
-        create_user.print(create_user.user)
+        print(user)
 
     elif initial_input == "2":
         check_login.check_root_privileges()
-        check_login.uname, password = check_login.get_user_credentials()
+        uname, password = check_login.get_user_credentials()
 
-        check_login.user = check_login.User(check_login.uname, password)
+        user = check_login.User(uname, password)
 
-        if check_login.user.authenticate():
+        if user.authenticate():
             print("Login successful.")
         else:
             print("Invalid Password or User does not exist.")
 
-    # elif initial_input == 3:
-    #     do this
+    elif initial_input == 3:
+        check_login.check_root_privileges()
+        uname, password = check_login.get_user_credentials()
+
+        user = check_login.User(uname, password)
+
+        if user.authenticate():
+            print("Login successful.")
+            check_login.check_root_privileges()
+            uname, password = check_login.get_user_credentials()
+
+            user = check_login.User(uname, password)
+
+            if user.authenticate():
+                # Request input: password
+                password = create_user.request_input(
+                    "Enter New Password for the user", "password"
+                )
+                re_password = create_user.request_input(
+                    "Re-enter New Password for the user", "password"
+                )
+
+            # Verify that the passwords match
+            if password != re_password:
+                print("Passwords do not match")
+                sys.exit()
+
+            # Request input: salt
+            salt = create_user.request_valid_salt()
+
+            # Create new user with the provided input
+            user = create_user.User(uname, password, salt)
+
+            # Print all the user info
+            print(user)
+        else:
+            print("Invalid Password or User does not exist.")
 
     # elif initial_input == 4:
     #     do this
